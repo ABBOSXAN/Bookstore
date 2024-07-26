df66fb45a6aa879ed68cdaabb29abc8bb9df5c59
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-07-26 17:15:48 +0500

    add static files

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 

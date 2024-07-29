50dcd887c1dea4c1d37671d3db5fc90d2bacf642
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-07-29 18:42:26 +0500

    updating

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+env=Env()
+env.read_env()
 # Application definition
 
 INSTALLED_APPS = [

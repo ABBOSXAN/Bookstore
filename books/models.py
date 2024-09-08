6f612eeb5807b9ac909aef8ed2fdb689b49e5344
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-08 10:17:03 +0500

    updated

--- books/models.py
+++ books/models.py
@@ -6,7 +6,11 @@ from django.urls import reverse
 # Create your models here.
 
 class Book(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid=models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False
+    )
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,5 @@ from .views import BookListView, BookDetailView
 
 urlpatterns=[
     path('', BookListView.as_view(), name='book_list'),
-    path('<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]

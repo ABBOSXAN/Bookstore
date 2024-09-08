76f2599fe8cb18f55ad6cf012a9cd67427f2332a
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-08 08:28:35 +0500

    updated

--- books/models.py
+++ books/models.py
@@ -6,11 +6,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Book(models.Model):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False  # id hech kim o'zgartirolmaydi
-    )
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -19,4 +15,4 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.id)])
+        return reverse('book_detail', args=[str(self.uuid)])

0a7bf496cb8653fc71fd2d2498d220b99c33296c
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-10 11:41:20 +0500

    updated

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -6,11 +7,6 @@ from django.urls import reverse
 # Create your models here.
 
 class Book(models.Model):
-    uuid=models.UUIDField(
-        default=uuid.uuid4,
-        primary_key=True,
-        editable=False
-    )
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -18,5 +14,21 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.uuid)])
+
+
+class Review(models.Model):
+    book=models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+
+    )
+    review=models.CharField(max_length=200)
+    author=models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review

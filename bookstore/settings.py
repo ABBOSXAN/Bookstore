9e544dcf49d9fdfcfddd041dea5a9f0ecf3678e5
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-14 11:56:31 +0500

    deploy update

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,7 +37,7 @@ CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ['https://bookstore-5im7.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bookstore-5im7.onrender.com', 'localhost', '127.0.0.1']
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[-1] + "1" for ip in ips]

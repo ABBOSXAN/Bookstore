1241d28a0133b29e5e56e7f2bf3c7d70b3502676
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-21 13:01:28 +0500

    updated

--- books/admin.py
+++ books/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from .models import ArtBook, TextBook, Review
+
+
 # Register your models here.
 
 class ReviewInline(admin.TabularInline):
     model = Review
 
-class BookAdmin(admin.ModelAdmin):
-    inlines = [ReviewInline,]
+
+class ArtBookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline, ]
     list_display = ('title', 'author', 'price')
 
 
-admin.site.register(ArtBook, TextBook, BookAdmin)
+class TextBookAdmin(admin.ModelAdmin):
+    list_display = ('title', "muallif", 'narx')
+
+
+admin.site.register(ArtBook, TextBook, ArtBookAdmin, TextBookAdmin)

160afa8ab46953ee088b3995ba1f05d2352973da
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-21 17:00:49 +0500

    updated

--- books/admin.py
+++ books/admin.py
@@ -13,4 +13,5 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price')
 
 
-admin.site.register(TextBook, BookAdmin)
+admin.site.register(ArtBook,TextBook)
+admin.site.register(BookAdmin)

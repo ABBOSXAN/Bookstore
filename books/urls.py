b4ca55faee98a57b97dc8ee051a3ccc800e9a7ff
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-09-21 11:43:07 +0500

    updated

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ArtBookListView, TextBookListView, BookDetailView, SearchResultsListView
 
 urlpatterns=[
-    path('artbook/<uuid:pk>', ArtBookListView.as_view(), name='art_book_list'),
+    path('', ArtBookListView.as_view(), name='art_book_list'),
     path('textbook/<uuid:pk>', TextBookListView.as_view(), name='text_book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
     path("search/", SearchResultsListView.as_view(), name='search_results'),
